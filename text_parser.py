b6bc3fe57bdc9ff4644e7175483880c2fbaf0f33
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-05-08 08:58:04 +0300

    Rework parse result

--- text_parser.py
+++ text_parser.py
@@ -1,11 +1,6 @@
 import os
 import re
-
-def make_view(items, start, end):
-    def get_items():
-        for i in range(start, end):
-            yield items[i]
-    return get_items
+from collections import namedtuple
 
 def split_to_parts(lines):
     anchors = []
@@ -19,31 +14,38 @@ def split_to_parts(lines):
     parts = []
     for i in range(0, len(anchors), 2):
         start, end = anchors[i], anchors[i + 1]
-        parts.append(make_view(lines, start, end))
+        parts.append(lines[start:end])
     return parts
 
 WEIGHT_PATTERN = re.compile(r'(\d+)$')
 RATIO_EXERCISE_PATTERN = re.compile(r'(\d+)%\s(\d+)[x|\*](\d+)')
 SIMPLE_EXERCISE_PATTERN = re.compile(r'(\d+)[x\*](\d+)')
 
-def parse_weights(lines_source):
-    weight_data = {}
-    for line in lines_source():
+WeightInfo = namedtuple('WeightInfo', ('name', 'weight'))
+
+def parse_weights(lines):
+    weights = []
+    for line in lines:
         match = WEIGHT_PATTERN.search(line.strip())
         if not match:
             raise Exception('bad line: ' + line)
         name = line[:match.start()].strip()
         weight = int(match.group(0))
-        weight_data[name] = weight
-    return weight_data
+        weights.append(WeightInfo(name, weight))
+    return weights
+
+RatioSetBlock = namedtuple('RatioSetBlock', ('ratio', 'reps', 'count'))
+SimpleSetBlock = namedtuple('SimpleSetBlock', ('reps', 'count'))
 
 def process_ratio_block(match):
     ratio, reps, count = map(int, match.groups())
-    return [{'reps': reps, 'ratio': ratio / 100.0}] * count
+    return RatioSetBlock(ratio / 100.0, reps, count)
 
 def process_simple_block(match):
     reps, count = map(int, match.groups())
-    return [{'reps': reps}] * count
+    return SimpleSetBlock(reps, count)
+
+Exercise = namedtuple('Exercise', ('name', 'sets'))
 
 def parse_exercise(text):
     pattern = None
@@ -63,32 +65,25 @@ def parse_exercise(text):
     name = text[:match.start()].strip()
     sets = []
     while match:
-        this_sets = process_block(match)
-        sets.extend(this_sets)
+        sets.append(process_block(match))
         match = pattern.search(text, match.end())
 
-    return {'name': name, 'sets': sets}
+    return Exercise(name, sets)
 
-def parse_day(lines_source):
-    lines = lines_source()
-    name = next(lines)
-    exercises = []
-    for line in lines:
-        exercises.append(parse_exercise(line.strip()))
-    return {'title': name, 'exercises': exercises}
+Day = namedtuple('Day', ('title', 'exercises'))
 
+def parse_day(lines):
+    name = lines[0]
+    exercises = [parse_exercise(line.strip()) for line in lines[1:]]
+    return Day(name, exercises)
+
+Plan = namedtuple('Plan', ('title', 'weights', 'days'))
 
 def parse(content):
     parts = split_to_parts(content.splitlines())
     if len(parts) < 3:
         raise Exception('not enough data')
-    title = os.linesep.join(parts[0]())
-    weight_data = parse_weights(parts[1])
-    days = []
-    for i in range(2, len(parts)):
-        days.append(parse_day(parts[i]))
-    return {
-        'title': title,
-        'weights': weight_data,
-        'days': days,
-    }
+    title = os.linesep.join(parts[0])
+    weights = parse_weights(parts[1])
+    days = [parse_day(parts[i]) for i in range(2, len(parts))]
+    return Plan(title, weights, days)
