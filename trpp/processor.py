c2b7081a77e365866aa7ce5d3bf94e4731152e11
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-05-14 09:36:21 +0300

    Add file processing

--- trpp/processor.py
+++ trpp/processor.py
@@ -1,4 +1,5 @@
 from io import TextIOWrapper
+from os import path
 from .text_parser import parse
 from .plan_renderer import render
 
@@ -9,3 +10,10 @@ def process(input_buffer, output_buffer):
     plan = parse(content)
     page = render(plan)
     output_wrapper.write(page)
+
+def process_file(file_name):
+    name, _ = path.splitext(path.basename(file_name))
+    output_name = path.join(path.dirname(file_name), name + '.html')
+    with open(file_name, mode='rb') as input_buffer:
+        with open(output_name, mode='wb') as output_buffer:
+            process(input_buffer, output_buffer)
