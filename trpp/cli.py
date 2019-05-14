6715633358b0dea997603ddf0e65f98aa9815f14
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-05-14 16:45:28 +0300

    Use '-' as std source mark

--- trpp/cli.py
+++ trpp/cli.py
@@ -6,7 +6,9 @@ def run(*args):
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('source', type=str, nargs='?', help='path to plan')
     args = parser.parse_args(*args)
-    if args.source:
+    if args.source == '-':
+        process(sys.stdin.buffer, sys.stdout.buffer)
+    elif args.source:
         process_file(args.source)
     else:
-        process(sys.stdin.buffer, sys.stdout.buffer)
+        parser.print_help()
