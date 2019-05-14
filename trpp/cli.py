a6dc4b3ab97a65ce9778d0193e0fd037a6ed40a2
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-05-14 16:56:19 +0300

    Add desciption

--- trpp/cli.py
+++ trpp/cli.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from .processor import process, process_file
 
 def run(*args):
-    parser = ArgumentParser(description=__doc__)
+    parser = ArgumentParser(description='Parses and renders training plan.')
     parser.add_argument('source', type=str, nargs='?', help='path to plan')
     args = parser.parse_args(*args)
     if args.source == '-':
